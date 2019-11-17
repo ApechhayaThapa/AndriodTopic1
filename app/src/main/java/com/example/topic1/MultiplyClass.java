2a489d8fff25efa959e732a77868b0456d1543d0
apechhaya <apputhapa@outlook.com>
2019-11-17 18:38:20 +0545

    Initial commit

---
+++ app/src/main/java/com/example/topic1/MultiplyClass.java
@@ -0,0 +1,30 @@
+package com.example.topic1;
+
+public class MultiplyClass {
+    private float firstvalue, secondvalue;
+
+    public float subtraction(float firstvalue, float secondvalue){
+
+        return firstvalue*secondvalue;
+
+    }
+
+    public float getFirstvalue() {
+        return firstvalue;
+    }
+
+    public void setFirstvalue(float firstvalue) {
+        this.firstvalue = firstvalue;
+    }
+
+    public float getSecondvalue() {
+        return secondvalue;
+    }
+
+    public void setSecondvalue(float secondvalue) {
+        this.secondvalue = secondvalue;
+    }
+
+
+
+}
